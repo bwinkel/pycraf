12507728102661654e47e44b63d4bc202cadf8cb
Benjamin Winkel <bwinkel@mpifr.de>
2017-06-30 17:58:51 +0200

    move itudata dir into pycraf package dir; otherwise pip wouldn't find it

--- README.md
+++ README.md
@@ -29,6 +29,26 @@ broadcasting tower.
 * Functions to convert power flux densities, field strengths, transmitted and
   received powers at certain distances and frequencies into each other.
 
+# License #
+
+`pycraf` is published under GPL v3, an open-source license.
+
+For some of the functionality provided in pycraf, data files provided by the
+ITU are necessary. For example, the atmospheric model in the pycraf.atm
+subpackage implements the algorithm described in ITU-R Recommendation P.676.
+Annex 1 of this Recommendation makes use of spectroscopic information of the
+oxygen and water vapour lines given in Tables 1 and 2 of P.676.
+
+ITU kindly gave us permission to include data files into pycraf that are
+distributed with the Recommendations on the ITU servers. This makes it possible
+to just use pycraf without the need to manually download necessary data files.
+However, these data files are not free for commercial use. For details, please
+see the LICENSE.ITU file in the `itudata` directory of the package.
+
+We are very grateful for the kind support from ITU study groups and ITU's legal
+department.
+
+
 # Usage #
 
 ## Installation ##
--- setup.py
+++ setup.py
@@ -27,79 +27,75 @@ ext_module_pathprof_geodesics = Extension(
     include_dirs=[np.get_include()],
     )
 
-try:
-    os.symlink('../itudata', 'pycraf/itudata')
-    setup(
-        name="pycraf",
-        version="0.22",
-        description="pycraf",
-        author="Benjamin Winkel",
-        author_email="bwinkel@mpifr.de",
-        url="https://github.com/bwinkel/pycraf",
-        download_url="https://github.com/bwinkel/pycraf/archive/0.22.tar.gz",
-        packages=[
-            'pycraf',
-            'pycraf.antenna',
-            'pycraf.atm',
-            'pycraf.conversions',
-            'pycraf.geospatial',
-            'pycraf.helpers',
-            'pycraf.pathprof',
-            'pycraf.protection',
-            ],
-        install_requires=[
-            'setuptools',
-            'numpy>=1.8',
-            # 'scipy>=0.15',
-            'astropy>=1.1',
-            'pyproj>=1.9',
-            ],
-        package_dir={
-            'pycraf': 'pycraf',
-            'pycraf.antenna': 'pycraf/antenna',
-            'pycraf.atm': 'pycraf/atm',
-            'pycraf.conversions': 'pycraf/conversions',
-            'pycraf.geospatial': 'pycraf/geospatial',
-            'pycraf.helpers': 'pycraf/helpers',
-            'pycraf.pathprof': 'pycraf/pathprof',
-            'pycraf.protection': 'pycraf/protection',
-            },
-        cmdclass={'build_ext': build_ext},
-        ext_modules=[
-            ext_module_pathprof_cyprop,
-            ext_module_pathprof_geodesics,
-            ],
-        package_data={
-            'pycraf': [
-                'itudata/README.md',
-                'itudata/LICENSE.ITU',
-                'itudata/p.676-10/R-REC-P.676-10-201309_table1.csv',
-                'itudata/p.676-10/R-REC-P.676-10-201309_table2.csv',
-                'itudata/ra.769-2/ra_769_table1_limits_continuum.csv',
-                'itudata/ra.769-2/ra_769_table2_limits_spectroscopy.csv',
-                'itudata/p.452-16/refract_map.npz',
-                'itudata/p.452-16/make_npy.py',
-                'itudata/p.452-16/R-REC-P.452-16-201507/DN50.TXT',
-                'itudata/p.452-16/R-REC-P.452-16-201507/LAT.TXT',
-                'itudata/p.452-16/R-REC-P.452-16-201507/LON.TXT',
-                'itudata/p.452-16/R-REC-P.452-16-201507/N050.TXT',
-                'itudata/p.452-16/R-REC-P.452-16-201507/ReadMe.doc',
-                ]
-            },
-        long_description='''pycraf ... the CRAF library.
-            Contains useful functions for the daily life of a spectrum manager
-            in RAS.
-            ''',
-        keywords=['pycraf', 'radio', 'compatibility study'],
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Operating System :: MacOS',
-            'Operating System :: POSIX :: Linux',
-            'Programming Language :: Python',
-            'Programming Language :: Cython',
-            'Topic :: Scientific/Engineering',
-            ],
-        )
-finally:
-    os.unlink('pycraf/itudata')
+setup(
+    name="pycraf",
+    version="0.23",
+    description="pycraf",
+    author="Benjamin Winkel",
+    author_email="bwinkel@mpifr.de",
+    url="https://github.com/bwinkel/pycraf",
+    download_url="https://github.com/bwinkel/pycraf/archive/0.23.tar.gz",
+    packages=[
+        'pycraf',
+        'pycraf.antenna',
+        'pycraf.atm',
+        'pycraf.conversions',
+        'pycraf.geospatial',
+        'pycraf.helpers',
+        'pycraf.pathprof',
+        'pycraf.protection',
+        ],
+    install_requires=[
+        'setuptools',
+        'numpy>=1.8',
+        # 'scipy>=0.15',
+        'astropy>=1.1',
+        'pyproj>=1.9',
+        ],
+    package_dir={
+        'pycraf': 'pycraf',
+        'pycraf.antenna': 'pycraf/antenna',
+        'pycraf.atm': 'pycraf/atm',
+        'pycraf.conversions': 'pycraf/conversions',
+        'pycraf.geospatial': 'pycraf/geospatial',
+        'pycraf.helpers': 'pycraf/helpers',
+        'pycraf.pathprof': 'pycraf/pathprof',
+        'pycraf.protection': 'pycraf/protection',
+        },
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        ext_module_pathprof_cyprop,
+        ext_module_pathprof_geodesics,
+        ],
+    package_data={
+        'pycraf': [
+            'itudata/README.md',
+            'itudata/LICENSE.ITU',
+            'itudata/p.676-10/R-REC-P.676-10-201309_table1.csv',
+            'itudata/p.676-10/R-REC-P.676-10-201309_table2.csv',
+            'itudata/ra.769-2/ra_769_table1_limits_continuum.csv',
+            'itudata/ra.769-2/ra_769_table2_limits_spectroscopy.csv',
+            'itudata/p.452-16/refract_map.npz',
+            'itudata/p.452-16/make_npy.py',
+            'itudata/p.452-16/R-REC-P.452-16-201507/DN50.TXT',
+            'itudata/p.452-16/R-REC-P.452-16-201507/LAT.TXT',
+            'itudata/p.452-16/R-REC-P.452-16-201507/LON.TXT',
+            'itudata/p.452-16/R-REC-P.452-16-201507/N050.TXT',
+            'itudata/p.452-16/R-REC-P.452-16-201507/ReadMe.doc',
+            ]
+        },
+    long_description='''pycraf ... the CRAF library.
+        Contains useful functions for the daily life of a spectrum manager
+        in RAS.
+        ''',
+    keywords=['pycraf', 'radio', 'compatibility study'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Cython',
+        'Topic :: Scientific/Engineering',
+        ],
+    )
