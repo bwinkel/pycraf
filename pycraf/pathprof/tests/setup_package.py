44f566ece88ed722136d3c6bee7f311ee96d3c62
bwinkel <bwinkel78@gmail.com>
2017-10-27 10:03:45 +0200

    integrate newest astropy package template

--- pycraf/pathprof/tests/setup_package.py
+++ pycraf/pathprof/tests/setup_package.py
@@ -1,2 +1,13 @@
+# import os
+
+# If this package has tests data in the tests/data directory, add them to
+# the paths here, see commented example
+# paths = [os.path.join('data', '*fits') ]
+
+# def get_package_data():
+#     return {
+#         _ASTROPY_PACKAGE_NAME_ + '.tests': paths}
+
+
 def get_package_data():
     return {'pycraf.pathprof.tests': ['*/*.*']}
