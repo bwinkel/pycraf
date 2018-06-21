bugfix in antenna.imt: a sign was wrong in composite pattern
patterns are now flipped with respect to azimuth (still positive Etilt
moves the beams downwards, instead of upwards?)

pycraf/antenna/imt.py
@@ -181,7 +181,7 @@ def _imt2020_composite_pattern(
 
         return np.exp(
             1j * 2 * np.pi * (
-                n * _dV_sin_theta_i +
+                n * _dV_sin_theta_i -
                 m * _dH_cos_theta_i_sin_phi_i
                 )
             ) / np.sqrt(N_H * N_V)

pycraf/antenna/tests/test_antenna.py
@@ -190,7 +190,7 @@ def test_imt2020_composite_pattern():
         atol=1.e-2 * cnv.dB, rtol=1.e-4,
         )
 
-    azim_i, elev_j = 30 * apu.deg, 15 * apu.deg
+    azim_i, elev_j = -30 * apu.deg, 15 * apu.deg
 
     gains_array = imt.imt2020_composite_pattern(
         azims[:, np.newaxis], elevs[np.newaxis],
@@ -248,7 +248,7 @@ def test_imt2020_composite_pattern_broadcast():
     azims = np.linspace(-50, 50, 3) * apu.deg
     elevs = np.linspace(-65, 65, 3) * apu.deg
 
-    azim_i, elev_j = [0, 10] * apu.deg, [0, 5] * apu.deg
+    azim_i, elev_j = [0, -10] * apu.deg, [0, 5] * apu.deg
 
     G_Emax = 5 * cnv.dB
     A_m, SLA_nu = 30. * cnv.dimless, 30. * cnv.dimless