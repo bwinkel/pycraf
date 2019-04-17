61dd7b285f18d7751eeb3f70bbaee7064a2fd92c
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-17 14:47:28 +0000

    Update docs after building Travis build 393 of bwinkel/pycraf
    The docs were built from the branch 'master' against the commit
    3193efaa1ab8350e24ce7dc3a03d4027e2c0daf4.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bwinkel/pycraf/jobs/521269640.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy --no-require-master --sync --built-docs docs/docs/_build/html latest

--- latest/atm/index-2.py
+++ latest/atm/index-2.py
@@ -1,48 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy import units as u
+from pycraf import atm
+from pycraf import conversions as cnv
+
 _freqs = np.arange(1, 1000, 1)
 freq_grid = _freqs * u.GHz
-# weather parameter at ground-level
 total_pressure = 1013 * u.hPa
 temperature = 290 * u.K
-# water content is from the integral over the full height
 rho_water = 7.5 * u.g / u.m ** 3
-# >>>
-# alternatively, one can use atm.pressure_water_from_humidity
+
 pressure_water = atm.pressure_water_from_rho_water(temperature, rho_water)
 pressure_dry = total_pressure - pressure_water
-# >>>
+
 print(
     'Oxygen pressure: {0.value:.2f} {0.unit}, '
     'Water vapor partial pressure: {1.value:.2f} {1.unit}'.format(
         pressure_dry, pressure_water
     ))
-# Oxygen pressure: 1002.96 hPa, Water vapor partial pressure: 10.04 hPa
+
 atten_dry, atten_wet = atm.atten_specific_annex1(
     freq_grid, pressure_dry, pressure_water, temperature
     )
-# ...
-plt.close()
-plt.figure(figsize=(12, 7))  # doctest: +IGNORE_OUTPUT
+
+plt.figure(figsize=(15, 9))
 plt.plot(
     _freqs, atten_dry.to(cnv.dB / u.km).value,
     'r-', label='Dry air'
-    )  # doctest: +IGNORE_OUTPUT
+    )
 plt.plot(
     _freqs, atten_wet.to(cnv.dB / u.km).value,
     'b-', label='Wet air'
-    )  # doctest: +IGNORE_OUTPUT
+    )
 plt.plot(
     _freqs, (atten_dry + atten_wet).to(cnv.dB / u.km).value,
     'k-', label='Total'
-    )  # doctest: +IGNORE_OUTPUT
-plt.semilogy()  # doctest: +IGNORE_OUTPUT
-plt.xlabel('Frequency [GHz]')  # doctest: +IGNORE_OUTPUT
-plt.ylabel('Specific Attenuation [dB / km]')  # doctest: +IGNORE_OUTPUT
-plt.xlim((1, 999))  # doctest: +IGNORE_OUTPUT
-plt.ylim((5.e-3, 0.9e5))  # doctest: +IGNORE_OUTPUT
-plt.grid()  # doctest: +IGNORE_OUTPUT
-plt.legend(*plt.gca().get_legend_handles_labels(), loc='upper left')  # doctest: +IGNORE_OUTPUT
+    )
+plt.semilogy()
+plt.xlabel('Frequency [GHz]')
+plt.ylabel('Specific Attenuation [dB / km]')
+plt.xlim((1, 999))
+plt.ylim((5.e-3, 0.9e5))
+plt.grid()
+plt.legend(*plt.gca().get_legend_handles_labels(), loc='upper left')
 plt.title(
     'Specific attenuation for standard conditions, '
     'according to ITU-R P.676 (10), annex 1',
     fontsize=16
-    )  # doctest: +IGNORE_OUTPUT
+    )
--- latest/atm/index-3.py
+++ latest/atm/index-3.py
@@ -1,7 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy import units as u
+from pycraf import atm
+from pycraf import conversions as cnv
+
+elevation = 5 * u.deg
 obs_alt = 300 * u.m
-_freqs = np.arange(0.25, 100, 0.5)
+_freqs = np.arange(0.1, 100, 1)
 freq_grid = _freqs * u.GHz
-# >>>
+
 cases = [
     # elevation, profile, label, linestyle
     (90 * u.deg, atm.profile_highlat_winter, 'Winter, Elevation: 90 deg', 'b-'),
@@ -9,42 +16,55 @@ cases = [
     (15 * u.deg, atm.profile_highlat_winter, 'Winter, Elevation: 15 deg', 'b--'),
     (15 * u.deg, atm.profile_highlat_summer, 'Summer, Elevation: 15 deg', 'r--'),
     ]
-# ...
-plt.close()
-fig, axes = plt.subplots(2, 2, figsize=(12, 12))
-for elev, profile, label, linestyle in cases:
-# ...
-    atm_layers_cache = atm.atm_layers(freq_grid, profile)
+
+fig = plt.figure(figsize=(15, 16))
+axes = [fig.add_subplot(2, 2, i) for i in range(1, 5)]
+for elevation, profile, label,linestyle in cases:
+
     total_atten, refraction, tebb = atm.atten_slant_annex1(
-        elev, obs_alt, atm_layers_cache, t_bg=2.73 * u.K
+        freq_grid, elevation, obs_alt, profile, t_bg=2.73 * u.K
         )
-    opacity = atm.opacity_from_atten(total_atten, elev)
-# ...
-    print('Refraction for {}: {:.1f}'.format(label, refraction.to(u.arcsec)))
-# ...
-    _ = axes[0, 0].plot(_freqs, total_atten.to(cnv.dB).value, linestyle, label=label)
-    _ = axes[0, 1].plot(_freqs, (-total_atten).to(cnv.dimless).value, linestyle, label=label)
-    _ = axes[1, 0].plot(_freqs, opacity.to(cnv.dimless).value, linestyle, label=label)
-    _ = axes[1, 1].plot(_freqs, tebb.to(u.K).value, linestyle, label=label)
-# Refraction for Winter, Elevation: 90 deg: -0.0 arcsec
-# Refraction for Summer, Elevation: 90 deg: -0.0 arcsec
-# Refraction for Winter, Elevation: 15 deg: -228.6 arcsec
-# Refraction for Summer, Elevation: 15 deg: -237.9 arcsec
-axes[0, 0].semilogy()  # doctest: +IGNORE_OUTPUT
-axes[1, 0].semilogy()  # doctest: +IGNORE_OUTPUT
-axes[0, 0].legend(*axes[0, 0].get_legend_handles_labels(), loc='upper left', fontsize=8)  # doctest: +IGNORE_OUTPUT
-axes[0, 0].set_ylabel('Total attenuation [dB]')  # doctest: +IGNORE_OUTPUT
-axes[0, 1].set_ylabel('Total gain')  # doctest: +IGNORE_OUTPUT
-axes[1, 0].set_ylabel('Zenith opacity')  # doctest: +IGNORE_OUTPUT
-axes[1, 1].set_ylabel('Tebb [K]')  # doctest: +IGNORE_OUTPUT
-axes[0, 0].set_ylim((2e-2, 9e2))  # doctest: +IGNORE_OUTPUT
-axes[0, 1].set_ylim((0, 1))  # doctest: +IGNORE_OUTPUT
-axes[1, 0].set_ylim((3e-3, 9e1))  # doctest: +IGNORE_OUTPUT
-axes[1, 1].set_ylim((0, 310))  # doctest: +IGNORE_OUTPUT
-# >>>
-for idx, ax in enumerate(axes.flat):
-    ax.grid()  # doctest: +IGNORE_OUTPUT
-    ax.set_xlim((1, 99))  # doctest: +IGNORE_OUTPUT
+    opacity = atm.opacity_from_atten(total_atten, elevation)
+
+    print(
+        'Refraction for {}: {:.1f} arcsec'.format(
+            label, refraction * 3600
+        ))
+
+    axes[0].plot(
+        _freqs, total_atten.to(cnv.dB).value,
+        linestyle, label=label
+        )
+    axes[1].plot(
+        _freqs, (-total_atten).to(cnv.dimless).value,
+        linestyle, label=label
+        )
+    axes[2].plot(
+        _freqs, opacity.to(cnv.dimless).value,
+        linestyle, label=label
+        )
+    axes[3].plot(
+        _freqs, tebb.to(u.K).value,
+        linestyle, label=label
+        )
+
+axes[0].semilogy()
+axes[2].semilogy()
+axes[0].legend(
+    *axes[0].get_legend_handles_labels(),
+    loc='upper left', fontsize=8
+    )
+axes[0].set_ylabel('Total attenuation [dB]')
+axes[1].set_ylabel('Total gain')
+axes[2].set_ylabel('Zenith opacity')
+axes[3].set_ylabel('Tebb [K]')
+axes[0].set_ylim((2e-2, 9e2))
+axes[1].set_ylim((0, 1))
+axes[2].set_ylim((3e-3, 9e1))
+axes[3].set_ylim((0, 310))
+
+for idx, ax in enumerate(axes):
+    ax.grid()
+    ax.set_xlim((1, 99))
     if idx >= 2:
-        ax.set_xlabel('Frequency [GHz]')  # doctest: +IGNORE_OUTPUT
-# ...
+        ax.set_xlabel('Frequency [GHz]')
