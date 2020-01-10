4305b83683c47f8e084f7d4b4ab2b4a7c5d59ccb
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-10 22:41:26 +0000

    Update docs after building Travis build 482 of bwinkel/pycraf
    The docs were built from the branch 'master' against the commit
    e368c3e4c55d9c49cbf6fb434d7e53dadec4c98d.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bwinkel/pycraf/jobs/635491372.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy --no-require-master --sync --built-docs docs/docs/_build/html latest

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.25.9.dev107',
+    VERSION: '1.0.1dev106',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
