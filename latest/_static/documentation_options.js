73e97a5d08dac32ea0dae9ca98b3c9c22662c9f4
Benjamin Winkel <bwinkel@mpifr.de>
2021-06-19 16:39:58 +0000

    Update docs for 1.0.5dev400 ***NO_CI***

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.5dev396',
+    VERSION: '1.0.5dev400',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
