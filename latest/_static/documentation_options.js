4ab046cb30bb24e4dae6052b47ece90c924e78e5
Benjamin Winkel <bwinkel@mpifr.de>
2022-11-22 15:25:12 +0000

    Update docs for 2.0.2.dev4+gc2e756c0 ***NO_CI***

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '',
+    VERSION: '2.0.2.dev4+gc2e756c0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
